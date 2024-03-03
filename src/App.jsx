930b654ffb90ade126ffdea62364105a3347e5ef
minseoyeo <minseoyeo13@gmail.com>
2024-03-03 21:00:49 +0900

    client side

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
 import Home from "./pages/Home";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./pages/Profile";
 
 function App() {
 
@@ -11,6 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignOut/>}/>
+
+        <Route element={<PrivateRoute/>}>
+          <Route path="/profile" element={<Profile/>}/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
