f4b4ac2f3a971ff9d3c7749ea9796d76296ca0bb
minseoyeo <minseoyeo13@gmail.com>
2024-03-07 12:44:11 +0900

    Sign In revision

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,5 @@
-import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice.js';
 import OAuth from '../components/OAuth';
@@ -7,14 +7,14 @@ import OAuth from '../components/OAuth';
 export default function SignIn() {
 
   const [formData, setFormData] = useState({});
-  const { loading, error} = useSelector((state) => state.user);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.id]: e.target.value
+      [e.target.id]: e.target.value,
     });
   };
 
@@ -28,7 +28,7 @@ export default function SignIn() {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData)
-      })
+      });
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
@@ -48,10 +48,24 @@ export default function SignIn() {
     <div className='p-3 max-w-lg max-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
       <form className='flex flex-col gap4' onSubmit={handleSubmit}>
-        <input type="text" placeholder='email' onChange={handleChange}/>
-        <input type="text" placeholder='password' onChange={handleChange}/>
+        <input 
+          type="email"
+          placeholder='email' 
+          onChange={handleChange}
+          id='email'
+        />
 
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>
+        <input 
+          type="password" 
+          placeholder='password' 
+          onChange={handleChange}
+          id='password'
+        />
+
+        <button 
+          disabled={loading} 
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'
+        >
           {loading ? 'Loading...' : 'Sign In'}
         </button>
         <OAuth/>
