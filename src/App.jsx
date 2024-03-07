51ebdc32b9a6d0b7eadb59192343a8f00de2a481
minseoyeo <minseoyeo13@gmail.com>
2024-03-07 12:30:20 +0900

    Sign up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import SignIn from "./pages/SignIn";
-import SignOut from "./pages/SignOut";
 import Home from "./pages/Home";
 import PrivateRoute from "./components/PrivateRoute";
 import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
 
 function App() {
 
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignOut/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
 
         <Route element={<PrivateRoute/>}>
           <Route path="/profile" element={<Profile/>}/>
