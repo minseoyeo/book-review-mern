1ea5b1e5ecf75f06edb977f8fa78c796f70faa8b
minseoyeo <minseoyeo13@gmail.com>
2024-03-19 14:14:13 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import PrivateRoute from "./components/PrivateRoute";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import CreateReview from "./pages/CreateReview";
+import About from "./pages/About";
+import Search from "./pages/Search";
+import Review from "./pages/Review";
+import UpdateReview from "./pages/UpdateReview";
 
 function App() {
 
@@ -16,10 +20,14 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/sign-in" element={<SignIn/>}/>
         <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/about" element={<About/>} />
+        <Route path="/search" element={<Search/>} />
+        <Route path="/review/:reviewId" element={<Review/>} />
 
         <Route element={<PrivateRoute/>}>
           <Route path="/profile" element={<Profile/>}/>
           <Route path="/create-review" element={<CreateReview/>} />
+          <Route path="/update-review/:reviewId" element={<UpdateReview/>} />
         </Route>
       </Routes>
     </BrowserRouter>
