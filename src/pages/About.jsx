0ee82a4aca0704f3f2eb0693d163f349c1a581d0
minseoyeo <minseoyeo13@gmail.com>
2024-03-19 13:56:35 +0900

    about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>
+        About Book Review
+      </h1>
+
+      <p className='mb-4 text-slate-700'>
+        Book Review is a leading book review website that specializes in helping users find, write, and share about books.      
+      </p>
+    </div>
+  )
+}
