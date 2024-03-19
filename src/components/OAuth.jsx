21edb131528f8e42a2a92d0fe7249886efa2a688
minseoyeo <minseoyeo13@gmail.com>
2024-03-19 13:15:11 +0900

    OAuth error correction

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -5,11 +5,9 @@ import { signInSuccess } from '../redux/user/userSlice.js';
 import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
-
+  const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const handleGoogleClick = async () => {
-    const dispatch = useDispatch();
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
@@ -19,22 +17,28 @@ export default function OAuth() {
       const res = await fetch('/api/auth/google', {
         method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ name: result.user.displayName, email: result.user.email, phto: result.user.photoURL}),
-      })
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
       const data = await res.json();
       dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      console.log('Could not sign in with google', error);
-    };
+      console.log('could not sign in with google', error);
+    }
   };
-
   return (
-    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-80'>
-      Continue with Google
+    <button
+      onClick={handleGoogleClick}
+      type='button'
+      className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'
+    >
+      Continue with google
     </button>
-  )
-
-};
+  );
+}
