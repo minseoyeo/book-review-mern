231efaa6ee4e5384ce989c34cd84b3da0e2137ed
minseoyeo <minseoyeo13@gmail.com>
2024-03-21 19:52:11 +0900

    revision

--- src/components/ReviewItem.jsx
+++ src/components/ReviewItem.jsx
@@ -4,10 +4,10 @@ import { MdLocationOn } from "react-icons/md";
 export default function ReviewItem({ review }) {
   return (
     <div className='bg-white shadow-md hover:shadow-lg transition-shadow rounded-lg w-full sm:-[330px]'>
-      <Link to={`/review/${review._id}`}>
+      <Link to={`/reviews/${review._id}`}>
         <h3 className='text-semibold text-lg text-slate-700 truncate'>{review.title}</h3>
         <h5 className='text-sm text-slate-600 truncate w-full'>{review.author}</h5>
-        <p className='text-sm text-gray-600'>{review.date}</p>
+        <p className='text-sm text-gray-600'>{review.rating}</p>
       </Link>
     </div>
   )
